6314ff0f952abf219ac466374a1ce404917f980c
Yasser Kaddoura <yasser.kaddoura19@gmail.om>
2021-05-22 11:32:05 +0200

    feat: add more features while extracting repos
    - amount of repos the owner have
    - main_language
    - followers user have (IF THE OWNER IS A USER, OTHERWISE IT'S 0)
    - commits comments user made (IF THE OWNER IS A USER, OTHERWISE IT'S 0)
    - is verified organization (IF THE OWNER IS AN ORGANIZATION, OTHERWISE IT'S 0)
    - members with roles in organization (IF THE OWNER IS AN ORGANIZATION, OTHERWISE IT'S 0)
    - read_me file size

--- dev/scripts/extract_repositories.py
+++ dev/scripts/extract_repositories.py
@@ -231,10 +231,27 @@ def get_repo_graphql_query(repo_id, owner, name):
     return query_string
 
 
+def update_repos_content(repos):
+    for repo_name in repos:
+        size = 0
+
+        if repos[repo_name]['readme_content_main'] is not None:
+            size = len(repos[repo_name]['readme_content_main'])
+
+        if repos[repo_name]['readme_content_master'] is not None:
+            size = len(repos[repo_name]['readme_content_master'])
+
+        repos[repo_name]['readme_size'] = size
+
+        del repos[repo_name]['readme_content_master']
+        del repos[repo_name]['readme_content_main']
+
+
 def main():
     repos = get_repos()
 
     add_repos_details_using_graphql(repos)
+    update_repos_content(repos)
 
     with open('repos.json', mode='w') as _file:
         json.dump(repos, _file, indent=6)
